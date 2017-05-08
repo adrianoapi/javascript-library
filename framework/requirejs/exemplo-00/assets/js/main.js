b72a2e960fe20f44ca006b2630f85ee845791a93
Adriano A Costa <sdcomputadores@gmail.com>
2017-05-08 15:58:21 -0300

    add app

--- framework/requirejs/exemplo-00/assets/js/main.js
+++ framework/requirejs/exemplo-00/assets/js/main.js
@@ -1,5 +1,11 @@
-function getCredits(){
-	console.log("Function: getCredits");
-	var credits = "100";
-	return credits;
-}
+require.config({
+    name: 'app',
+    baseUrl: 'assets/js',
+    paths: {
+        'jquery': 'libs/jquery-3.2.1.min'
+    }
+});
+
+require(['jquery'], function ($) {
+    $('body').append('String invocada');
+});
