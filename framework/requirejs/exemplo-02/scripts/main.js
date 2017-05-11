46d96b89b00f53217aa0dcbd5ef4dc4ca9133142
Adriano A Costa <sdcomputadores@gmail.com>
2017-05-11 14:58:17 -0300

    reposicionamento do style.css e add exemplo-02

---
+++ framework/requirejs/exemplo-02/scripts/main.js
@@ -0,0 +1,29 @@
+requirejs.config({
+    paths: {
+        "jquery": "lib/jquery-3.2.1.min",
+        "Pessoa": "lib/pessoa"
+    }
+});
+
+require(["jquery", "Pessoa"], function ($, Pessoa) {
+    Pessoa.prototype.setName('Adriano');
+
+    var $form = $("#form"),
+            $email = $("#email"),
+            nome = Pessoa.prototype.getName();
+    console.log(nome);
+    
+    $form.on("submit", function (e) {
+        e.preventDefault();
+        require(["lib/validation-plugin"], function () {
+            if ($email.isValidEmail()) {
+                $form.get(0).submit();
+            } else {
+                $email.addClass("error").focus();
+            }
+        });
+    });
+    $email.on("keyup", function () {
+        $email.removeClass("error");
+    });
+});
